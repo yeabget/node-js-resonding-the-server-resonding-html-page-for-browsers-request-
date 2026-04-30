eb479175f624a120a1a275945767fabb363f44c7
unknown <yabget19@gmail.com>
2026-04-30 18:23:19 +0300

    routing by using express js

--- hello.js
+++ hello.js
@@ -1,4 +1,9 @@
-const greet=()=>{
-    return "helo world"
-}
-module.exports=greet;
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/view/index.html')
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(__dirname+'/view/about.html')
+})
+app.listen(3000)
