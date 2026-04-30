7b95ab63fe3514cc4e69062f591c20ade9baee63
unknown <yabget19@gmail.com>
2026-04-30 23:21:02 +0300

    simple web page by ejs includes routing

--- hello.js
+++ hello.js
@@ -1,9 +1,29 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/view/index.html')
-})
-app.get('/about',(req,res)=>{
-    res.sendFile(__dirname+'/view/about.html')
-})
-app.listen(3000)
+const express = require('express');
+const app = express();
+const blogs=[{title:"Abebe",desc:"this is abebe from health care management"},
+    {title:"Kebede",desc:"this is kebede from ethio telecom"},
+    {title:"Alemitu",desc:'this is alemitu from ethiopian electricity coporation'}
+
+]
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { 
+        title: "Home",
+        blogs  
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about',{title:"about"});
+});
+app.get('/contact',(req,res)=>{
+    res.render('contact',{title:"contact"});
+});
+app.use((req, res) => {
+    res.status(404).render('404',{title:"error"});
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
