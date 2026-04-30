f1e2b84bbeba1e163f945c0803a83fad9481bd95
unknown <yabget19@gmail.com>
2026-04-30 15:00:45 +0300

    routing to different pages by using server responds different html pages based on the request

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const fs=require('fs')
 const http=require('http')
+const fs=require('fs')
 const server=http.createServer((req,res)=>{
     res.setHeader('content-Type','text/html')
-    fs.readFile("./view/index.html",(err,data)=>{
+    let path;
+    if(req.url==='/'){
+        path='./view/index.html'
+    }
+    else if(req.url==='/about'){
+        path='./view/about.html'
+    }
+    else if(req.url==='/contact'){
+        path='./view/contact.html'
+    }
+     else{
+        path='./view/404.html'
+     }
+     fs.readFile(path,(err,data)=>{
         if(err){
-            console.log(err);
+            console.log(err)
+
         }
         else{
-        res.end(data);
-        console.log("server is responding")
+            res.end(data)
+            console.log("responding to the request")
         }
-    })
+     })
 })
 server.listen(3000,()=>{
-    console.log('server is listining port 3000')
-});
+    console.log("server is listning o the port 3000")
+})
